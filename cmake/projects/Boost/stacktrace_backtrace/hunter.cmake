0817a5524ffb648d2424395a4f130880e5626744
Suckow, Thomas J <Thomas.Suckow@pnnl.gov>
2020-12-22 16:29:41 -0800

    Add stacktrace_backtrace

---
+++ cmake/projects/Boost/stacktrace_backtrace/hunter.cmake
@@ -0,0 +1,19 @@
+# !!! DO NOT PLACE HEADER GUARDS HERE !!!
+
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_pick_scheme(
+    DEFAULT
+    url_sha1_boost_library
+    IPHONEOS
+    url_sha1_boost_ios_library
+)
+
+hunter_download(
+    PACKAGE_NAME
+    Boost
+    PACKAGE_COMPONENT
+    stacktrace_backtrace
+    PACKAGE_INTERNAL_DEPS_ID "48"
+)
